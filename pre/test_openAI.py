610f44755ce60a08cf65a76bae9c701ffdc9110d
shiunlee <shiun147@gmail.com>
2024-11-05 16:32:19 +0900

    프롬프트 분석항목 추가

--- pre/test_openAI.py
+++ pre/test_openAI.py
@@ -22,7 +22,7 @@ def extract_information_with_gpt(cleaned_html, api_key):
 
     prompt = f"""
     아래는 정제된 웹 페이지의 HTML입니다. 이 HTML에서 다음 정보를 추출하여 JSON 형식으로 반환하세요:
-    - 접근권한 (access_permission) : 만약 접근에 제한이 있는 페이지로 확인된다면 '에러코드', '없는 페이지', '로그인 필요' 등의 적절한 항목을 출력. 문제가 없다면 '정상'을 출력
+    - 접근권한 (access_permission) : 만약 접근에 제한이 있는 페이지로 확인된다면 'HTTP 상태 코드', '없는 페이지', '로그인 필요' 등의 적절한 항목을 출력. 문제가 없다면 '정상'을 출력
     - 호스트 도메인 (host_domain) :
     - 호스트 이름 (host_name) : 호스트의 이름을 한글로 출력, 한글이 없다면 영어로 출력 (예시: 네이버 뉴스, 페이스북, 위키백과)
     - 대체 url (alternate_url) : Canonical URL 또는 Open Graph URL이 존재 한다면 이곳에 표시
@@ -33,9 +33,13 @@ def extract_information_with_gpt(cleaned_html, api_key):
     - 짧은 요약 (short_summary) : 본문을 20자에서 90자로 사이로 요약
     - 긴 요약 (long_summary) : 본문을 200자에서 400자 사이로 요약
     - 키워드 (keywords) : 본문의 키워드들 내용이 길고 요소가 많다면 키워드들이 아주 많아져도 좋아
-    - 이미지 링크 (image_links) : 주요 이미지들의 링크 주소
     - 유형 키워드 (category_keywords) : 블로그, 카페, 기사, 정보, 사연, 에세이, 영상, 사진, 리뷰, 쇼핑, sns 등 유형이라 할 수 있는 키워드들 풍부하게
     - 댓글 (comments) : 댓글이 있다면 '작성자 | 댓글내용 | 작성시간' 형식으로 저장
+    - 이미지 링크 (image_links) : 주요 이미지들의 링크 주소 {{"이미지캡션" : "이미지링크url"}} 딕셔너리 형식으로 저장
+    - 링크(links) : 본문 내에 주요한 외부 또는 내부 링크들이 있을경우 {{"캡션" : "링크url"}} 딕셔너리 형식으로 저장
+    - 비디오 및 오디오(media) : 본문 내에 미디어가 있을경우 {{"캡션" : "링크url"}} 딕셔너리 형식으로 저장
+    - 파일 다운로드 링크(file_download_links) : 주요한 PDF, 이미지, 문서 등의 다운로드 링크가 있을경우 {{"파일제목 | 파일 크기": "링크url"}} 딕셔너리 형식으로 저장
+    - 콘텐츠의 길이(content_length) : 콘텐츠를 읽는데 걸리는 시간을 예측 1분 단위로
 
 
 
@@ -57,9 +61,13 @@ def extract_information_with_gpt(cleaned_html, api_key):
         "short_summary": "본문을 20자에서 90자로 요약한 내용",
         "long_summary": "본문을 200자에서 400자로 요약한 내용",
         "keywords": ["키워드1", "키워드2"],
-        "image_links": ["https://example.com/image1.jpg", "https://example.com/image2.jpg"],
         "category_keywords": ["블로그", "카페"],
-        "comments": ["작성자1 | 댓글내용1 | YYYY-MM-DD", "작성자2 | 댓글내용2 | YYYY-MM-DD"]
+        "comments": ["작성자1 | 댓글내용1 | YYYY-MM-DD", "작성자2 | 댓글내용2 | YYYY-MM-DD"],
+        "image_links": {{"이미지캡션1": "https://example.com/image1.jpg", "이미지캡션2": "https://example.com/image2.jpg" }},
+        "links": {{"캡션1": "https://example.com/image1.jpg", "캡션2": "https://example.com/image2.jpg" }},
+        "media_links": {{"캡션1": "https://example.com/image1.jpg", "캡션2": "https://example.com/image2.jpg" }},
+        "file_download_links": {{"캡션1 | 10MB": "https://example.com/image1.jpg", "캡션2 | 1.1GB": "https://example.com/image2.jpg" }},
+        "content_length": "MM"
     }}
 
     모든 응답은 유효한 JSON 형식이어야 하며, 추가적인 텍스트는 포함하지 마세요.
