9acd598a95c1fdb78bb220ba5bae1a7af7955924
shiunlee <shiun147@gmail.com>
2024-11-05 15:28:54 +0900

    프롬프트 추가

--- pre/url_to_html.py
+++ pre/url_to_html.py
@@ -1,6 +1,6 @@
 import requests
 
 # url = 'https://devyihyun.tistory.com/166'
-url = 'https://www.donga.com/news/Opinion/article/all/20241104/130359895/2'
-# url = 'https://brunch.co.kr/@xuang/27'
+# url = 'https://www.donga.com/news/Opinion/article/all/20241104/130359895/2'
+url = 'https://brunch.co.kr/@xuang/27'
 # url = 'https://www.10000recipe.com/recipe/6927963?srsltid=AfmBOoqPM8P79bR3-gfkyNHwf_JX-wv_XTphu2Zt1nfMJFEr250SnRrp'
